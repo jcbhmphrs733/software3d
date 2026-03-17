9125a05514bcb9d29ff83b683e47ed97ef34ebc1
EduardOrellana <eduardorellana13@gmail.com>
2026-03-16 20:34:34 -0600

    Mesheses

--- src/main.cpp
+++ src/main.cpp
@@ -4,44 +4,48 @@
 #include "math/vec3.h"
 #include "mesh/obj_loader.h"
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 void processInput(GLFWwindow *window);
 
 const int WINDOW_WIDTH = 800;
 const int WINDOW_HEIGHT = 600;
-const char* WINDOW_TITLE = "Software Rasterizer";
+const char *WINDOW_TITLE = "Software Rasterizer";
 GLuint shaderProgram, VAO, VBO;
 
 const char *vertexShaderSource = "#version 330 core\n"
-    "layout (location = 0) in vec3 aPos;\n"
-    "void main()\n"
-    "{\n"
-    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-    "}\0";
+                                 "layout (location = 0) in vec3 aPos;\n"
+                                 "void main()\n"
+                                 "{\n"
+                                 "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+                                 "}\0";
 
 const char *fragmentShaderSource = "#version 330 core\n"
-    "out vec4 FragColor;\n"
-    "void main()\n"
-    "{\n"
-    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-    "}\n\0";
+                                   "out vec4 FragColor;\n"
+                                   "void main()\n"
+                                   "{\n"
+                                   "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+                                   "}\n\0";
 
-GLFWwindow* InitializeWindow() {
-    if (!glfwInit()) return nullptr;
+GLFWwindow *InitializeWindow()
+{
+    if (!glfwInit())
+        return nullptr;
 
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Software Rasterizer", NULL, NULL);
-    if (!window) {
+    GLFWwindow *window = glfwCreateWindow(800, 600, "Software Rasterizer", NULL, NULL);
+    if (!window)
+    {
         glfwTerminate();
         return nullptr;
     }
 
     glfwMakeContextCurrent(window);
 
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
         std::cerr << "Failed to initialize GLAD\n";
         return nullptr;
     }
@@ -49,48 +53,47 @@ GLFWwindow* InitializeWindow() {
     return window;
 }
 
-void SetupResources() {
+void SetupResources()
+{
 
-        GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-        glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-        glCompileShader(vertexShader);
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
 
-        GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-        glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-        glCompileShader(fragmentShader);
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
 
-        shaderProgram = glCreateProgram();
-        glAttachShader(shaderProgram, vertexShader);
-        glAttachShader(shaderProgram, fragmentShader);
-        glLinkProgram(shaderProgram);
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
 
-        glDeleteShader(vertexShader);
-        glDeleteShader(fragmentShader);
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
 
-        float vertices[] = {
-             0.0f,  1.0f, 0.0f,
-            -0.5f, -0.5f, 0.0f,
-             0.5f, -0.5f, 0.0f
-        };
+    float vertices[] = {
+        0.0f, 1.0f, 0.0f,
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f};
 
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
 
-        glGenVertexArrays(1, &VAO);
-        glGenBuffers(1, &VBO);
+    glBindVertexArray(VAO);
 
-        glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+    glEnableVertexAttribArray(0);
 
-        glBindBuffer(GL_ARRAY_BUFFER, VBO);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-        glEnableVertexAttribArray(0);
-
-        glBindBuffer(GL_ARRAY_BUFFER, 0);
-        glBindVertexArray(0);
-
-
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
 }
-void RunRenderLoop(GLFWwindow* window) {
-while (!glfwWindowShouldClose(window)) {
+void RunRenderLoop(GLFWwindow *window)
+{
+    while (!glfwWindowShouldClose(window))
+    {
         processInput(window);
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
@@ -105,24 +108,24 @@ while (!glfwWindowShouldClose(window)) {
     }
 }
 
-
-
-void Cleanup(GLFWwindow* window) {
+void Cleanup(GLFWwindow *window)
+{
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
     glDeleteProgram(shaderProgram);
     glfwTerminate();
 }
 
-int main() {
-
+int main()
+{
 
     // --- ObjLoader test ---
     ObjLoader loader;
     Mesh cube = loader.load("../assets/models/cube.obj");
 
     std::cout << "Vertices loaded: " << cube.vertices.size() << "\n";
-    for (const Vec3& v : cube.vertices) {
+    for (const Vec3 &v : cube.vertices)
+    {
         std::cout << "  v " << v.x << " " << v.y << " " << v.z << "\n";
     }
 
@@ -130,22 +133,25 @@ int main() {
     std::cout << "Triangles: " << cube.indices.size() / 3 << "\n";
     // --- end test ---
 
-    GLFWwindow* window = InitializeWindow(); 
-    if (!window) {
+    GLFWwindow *window = InitializeWindow();
+    if (!window)
+    {
         return -1;
     }
-    SetupResources(); 
+    SetupResources();
     RunRenderLoop(window);
     Cleanup(window);
 
     return 0;
 }
 
-void processInput(GLFWwindow *window) {
+void processInput(GLFWwindow *window)
+{
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
 }
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+{
     glViewport(0, 0, width, height);
 }
